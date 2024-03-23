31de3fc060f5eb0d2b9e190387cecafbc0e2a4f3
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-03-23 05:18:06 +0100

    Add Keyboard component to project

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,28 @@
+import { observer } from "mobx-react-lite";
+
+export default observer(function Keyboard({ store }: { store: WordleStoreType }) {
+    const alphabet = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+    return (
+        <div>
+            {alphabet.map((row, i) => {
+                return (
+                    <div key={i} className="flex justify-center">
+                        {row.split('').map((letter, j) => {
+                            const bgColor = 
+                            {...store}.correctLetters.includes(letter) ? 'bg-green-500' : 
+                            {...store}.misplacedLetters.includes(letter) ? 'bg-yellow-500' : 
+                            {...store}.usedLetters.includes(letter) ? 'bg-gray-600' : 'bg-gray-200';
+                            return (
+                                <>
+                                    { i === 2 && j === 0 && <div key="enter" onClick={() => store.handleKeydown('Enter')} className={`m-2 p-4 bg-gray-200 rounded-sm font-bold cursor-pointer`}>ENTER</div> }
+                                    <div key={j} onClick={() => store.handleKeydown(letter)} className={`m-2 p-4 ${bgColor} rounded-sm font-bold cursor-pointer`}>{letter.toUpperCase()}</div>
+                                    { i === 2 && j === row.length - 1 && <div key="backspace" onClick={() => store.handleKeydown('Backspace')} className={`m-2 p-4 bg-gray-200 rounded-sm font-bold cursor-pointer`}>DEL</div> }
+                                </>
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </div>
+    )
+})
