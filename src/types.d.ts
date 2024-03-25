5ed4aa71b01998ac6f8f88b89ad7ceb7ea2c060c
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-03-25 05:09:14 +0100

    Update WordleStoreType in types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,20 @@
 type WordleStoreType = {
     guesses: Array<Array<GuessType>>,
-    currentGuess: number,
+    maxGuesses: number,
+    currentGuessIndex: number,
     wordLength: number,
     currentWordLength: number,
     allowDuplicates: boolean,
     correctLetters: Array<string>,
     misplacedLetters: Array<string>,
     usedLetters: Array<string>,
+    hasWon: boolean,
+    hasLost: boolean,
     init: () => void,
     submitGuess: (guess: string) => void,
     handleKeydown: (key: string) => void,
-    setWordLength: (length: number) => void
+    setWordLength: (length: number) => void,
+    resetGame: () => void
 }
 
 type GuessType = {
